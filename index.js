9219c1c2e4e9bbd97afe76f520b0f4b0ec4d0605
Irina <il064330@gmail.com>
2023-05-02 22:45:23 +0300

    create files

---
+++ index.js
@@ -0,0 +1,40 @@
+let cleaner = 40000;
+let teacher = 22000;
+console.log(cleaner + teacher); 
+
+let city = "Владивосток";
+let year = 2000;
+console.log(city + " " + year);
+
+let chat = 128;
+let like = 254;
+let read = 137;
+let watch = 201;
+let minutes = 60;
+console.log( (chat + like + read + watch) / minutes);
+
+let a = 2;
+let b = 5;
+console.log(2**5);
+
+let quote = "Нужно бежать со  всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(quote)
+
+const greetings = "Привет";
+const name = " Кот";
+console.log(`${greetings}, ${name}!`);
+
+let time 
+console.log(time);
+time = 34;
+console.log(`Старт поездки. осталось минут: ` + time);
+time -=15;
+console.log(`Немного сториз в соцсетях. Осталось минут ` + time);
+time -=10;
+console.log(`Немного не новостей, но событий. осталось минут: ` + time);
+time = 0;
+console.log(`Вы приехали. Добро пожаловать в Москву.`);
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`)
